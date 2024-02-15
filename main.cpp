a8b937a4dfa3de6220ca632b88a3dad93aba1be2
Shantam Sridev <50525078+ShantamSridev@users.noreply.github.com>
2024-02-15 14:15:44 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,272 +1,79 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include "pindefs.h"
+#include <cstdio>
 
-int main() {
-    stdio_init_all();
-    const uint LED_PIN = 25;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
+//#include "Ringbuffer.h"
+#include "pwm.h"
+#include "onewire.h"
+#include "encoder.h"
+#include "velocity.h"
+#include "current.h"
 
-    }
-}
-
-#include "Ringbuffer.h"
-//#include <util/atomic.h>  // For the ATOMIC_BLOCK macro
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 #include "hardware/gpio.h"
-#include "pico-onewire/api/one_wire.h"
+#include "hardware/timer.h"
+#include "hardware/adc.h"
 
-#define ENCA 2  // WHITE
-#define ENCB 3  // YELLOW
-#define PWM 5   //Output
-#define ONE_WIRE_BUS 4 //Digital 4
-#define CURRENT 1
-#define PWM_PIN 15
+// #include <OneWire.h>
+// #include <DallasTemperature.h>
 
-OneWire oneWire(ONE_WIRE_BUS);  
-DallasTemperature sensors(&oneWire);
-
-
-int serialInfo = 0;
-int dutyCycle = 0;
-int increment = 0;
-
-
-int i = 100;
-int x = 1;
-
+int position = 0;
 // globals
-long prevT = 0;
-int posPrev = 0;
+long prevTime= 0;
+int positionPrev = 0;
 
-volatile int pos = 0;
-volatile int pos_i = 0;
-
-volatile float velocity = 0;
-//volatile float velocity_i = 0;
-//volatile long prevT_i = 0;
-
-float deriv = 0;
-float eintegral = 0;
-float eprev = 0;
-
-float v1Filt = 0;
-float v1Prev = 0;
-
-float temp = 0;
+// OneWire oneWire(ONE_WIRE_BUS);  
+// DallasTemperature sensors(&oneWire);
 
 
-void setup() {
-  //stdio_init_all();
-  Serial.begin(9600);
-  sensors.begin();
-
-  pinMode(ENCA, INPUT);
-  digitalWrite(ENCA, HIGH);       // turn on pullup resistor
-  pinMode(ENCB, INPUT);
-  digitalWrite(ENCB, HIGH);       // turn on pullup resistor
-  pinMode(PWM, OUTPUT);
-
-  //PWMtimer_setup();
-
-  attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING);
+void isr_pin_a(uint gpio, uint32_t events) {
+    bool stateB = gpio_get(ENCB); // Read the current state of pin B
+    int i = 0;
+    if(stateB>0) {
+        i++; // Moving in one direction
+    } else {
+        i--; // Moving in the opposite direction
+    }
+    position = position +i;
 }
 
-void loop() {
-  temp = readTemp();
+void init_encoder() {
+    // Initialize GPIO pins for encoder A and B as inputs
+    gpio_init(ENCA);
+    gpio_set_dir(ENCA, GPIO_IN);
+    gpio_pull_up(ENCA); // Optional: enable pull-up if necessary
 
-  float current = readCurrent();
+    gpio_init(ENCB);
+    gpio_set_dir(ENCB, GPIO_IN);
+    gpio_pull_up(ENCB); // Optional: enable pull-up if necessary
 
-  // serialInfo = Serial.parseInt();
-  // if (serialInfo > 0) {
-  //   dutyCycle = serialInfo;
-  // }
-  
-  // dutyCycle = i;
-  // if (i>150){
-  //   x = -1;
-  // }
-  // if(i<52){
-  //   x= 1;
-  // }
-  // i = i+x;
-  // delay(50);
-
-
-//this works right
-
-  //ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    pos = pos_i;
-  //}
-
-   // Compute velocity
-  long currT = micros();
-  float deltaT = ((float) (currT-prevT));
-  float velocity = ((pos - posPrev)*(1.0e6))/deltaT;
-  //Gearbox Correction
-  velocity = velocity/19.2;
-
-  //i think the gear band ratio is 1:1.6
-
-  posPrev = pos;
-  prevT = currT;
-
-  // // Low-pass filter
-  // v1Filt = 0.854*v1Filt + 0.0728*velocity + 0.0728*v1Prev;
-  // v1Prev = velocity;
-
-//##############################################################
-  //CONTROL ALGORITHM
-  float vtarget = 30;
-
-  // Constants
-  float kp = 1.5;
-  float kd = 0;
-  float ki = 1;
-
-  //error
-  float error = vtarget - v1Filt;
-  deriv = (v1Filt-v1Prev)/(deltaT);
-  eintegral = eintegral + error*deltaT;
-  
-  float u = kp*error + kd*deriv + ki*eintegral;
-
-  int pwr = ((int) fabs(u));//+dutyCycle;
-  if(pwr > 150){
-    pwr = 150;
-  }
-
-//##############################################################
-
-  //Motor Outputs
-  //dutyCycle = map(pwr,0,40,100,150);//map(u,0,800,100,150);
-
-  // Serial.print("Serial : ");
-  //Serial.println(dutyCycle);
-  // Serial.print("Velocity : ");
-  // Serial.println(velocity);
-  // Serial.print("Pos : ");
-  // Serial.println(pos);
-
-  // Serial.print(vtarget);
-  // Serial.print(" ");
-  // Serial.print(v1Filt);
-  // Serial.println();
-  // delay(1);
-
-  // Initialize the PWM
-  float dutyCyclePercent = 50.0; // Set the initial duty cycle to 50%
-  initPWM(PWM_PIN, dutyCyclePercent);
-  //##############################TO BE CHECKED#########################
-
-
-  Serial.print("  ");
-  Serial.print(50);
-  Serial.print(" ");
-  Serial.print(v1Filt);
-  Serial.print(" ");
-  Serial.print(velocity);
-  Serial.print(" ");
-  Serial.print(0);
-  Serial.println();
-  delay(1);
-
-
-  // Serial.print(150);
-  // Serial.print(" ");
-  // Serial.print(dutyCycle);
-  // Serial.print(" ");
-  // Serial.print(100);
-  // Serial.println();
-  // delay(1);
-
-  eprev = error;
+    // Configure interrupt for pin A to trigger on both rising and falling edges
+    gpio_set_irq_enabled_with_callback(ENCA, GPIO_IRQ_EDGE_RISE, true, &isr_pin_a);
 }
 
 
-void readEncoder() {
-  int b = digitalRead(ENCB);
-  if(b>0){
-    increment = 1;
-  }
-  else{
-    increment = -1;
-  }
-  pos_i = pos_i + increment;
-}
+int main() {
+  // Initialize stdio for output
+    stdio_init_all();
 
-// void PWMtimer_setup() {
-//   // Set up Timer 1 for 50Hz PWM
-//   TCCR1A = 0;
-//   TCCR1B = 0;
-//   TCNT1 = 0;
-//   OCR1A = 39999;            // 16MHz / (50Hz * 8) - 1
-//   TCCR1B |= (1 << WGM12);   // CTC mode
-//   TCCR1B |= (1 << CS11);    // Prescaler 8
-//   TIMSK1 |= (1 << OCIE1A);  // Enable timer compare interrupt
-// }
+    init_pwm();
+    init_onewire();
+    init_encoder();
+    //sensors.begin();
+    init_currentsensor();
 
-// ISR(TIMER1_COMPA_vect) {
-//   // Calculate duty cycle
-//   int pulseWidth = map(dutyCycle, 0, 4000, 0, OCR1A);
-
-//   // Generate PWM signal
-//   digitalWrite(PWM, HIGH);
-//   delayMicroseconds(pulseWidth);
-//   digitalWrite(PWM, LOW);
-// }
-
-void initPWM(uint gpio, float dutyCyclePercent) {
-    // Calculate the PWM slice number from the GPIO pin
-    uint sliceNum = pwm_gpio_to_slice_num(gpio);
-
-    // Set up PWM for 50Hz
-    // The RP2040 has a system clock of 125MHz. The PWM clock divider can be adjusted to achieve different frequencies.
-    // To achieve 50Hz: divider = clock / (frequency * TOP + 1), where TOP is the PWM counter's top value (max value).
-    // We choose a TOP value that allows for enough resolution while still being able to achieve 50Hz.
-    uint32_t topValue = 125000; // This value might need adjustment for fine tuning
-    pwm_set_wrap(sliceNum, topValue);
-    float clockDiv = 125000000.0f / (50 * (topValue + 1));
-
-    // Set the PWM clock divider and enable PWM
-    pwm_set_clkdiv(sliceNum, clockDiv);
-    pwm_set_enabled(sliceNum, true);
-
-    // Configure the duty cycle (percentage)
-    uint32_t dutyCycle = (uint32_t)((dutyCyclePercent / 100.0) * (topValue + 1));
-    pwm_set_gpio_level(gpio, dutyCycle);
-
-    // Set the GPIO to be used by the PWM
-    gpio_set_function(gpio, GPIO_FUNC_PWM);
-}
-
-
-float readTemp() {
-    // Send the command to get temperatures
-  sensors.requestTemperatures(); 
-
-  //print the temperature in Celsius
-  //Serial.print("Temperature: ");
-  float newtemp = sensors.getTempCByIndex(0);
-  if (newtemp>0){
-    return newtemp;
-  }
-  else{
-    return 0;
-  }
-  //Serial.print(newtemp);
-  //Serial.print("C  |  ");
-}
-
-float readCurrent(){
-  float val = analogRead(CURRENT);  // read the input pin
-  float current = (val - 516)*39/512; //CURRENT SENSOR
-
-  //Serial.print("Current: ");
-  //Serial.print(current);
-  return current;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    
+    // Main loop
+    while (true) {
+        pwm_out(30);
+        gpio_put(LED_PIN, 1);
+        //printf("Check\n");
+        //printf("Encoder: %d\n",position);
+        //printf("Angle: %f\n",(float(position)/135)*360);
+        printf("Velocity: %f\n", calc_velocity(position, prevTime, positionPrev));
+        
+        sleep_ms(100);
+    }
 }
