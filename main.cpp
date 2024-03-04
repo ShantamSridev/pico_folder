31faddc4905c1369c5c4a8d9a3103c5223921539
Shantam Sridev <50525078+ShantamSridev@users.noreply.github.com>
2024-03-04 12:10:48 +0000

    Working wihtout uart

--- main.cpp
+++ main.cpp
@@ -11,22 +11,61 @@
 #include "hardware/adc.h"
 #include "hardware/i2c.h"
 #include <stdio.h>
-#include "pico/multicore.h"
 
+
+/// \tag::voltmeter-i2c[]
+
+#define UART_ID uart1
+#define BAUD_RATE 115200
+#define DATA_BITS 8
+#define STOP_BITS 1
+#define PARITY    UART_PARITY_NONE
+
+// UART pins
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
 
 #define I2C_ADDR 0x3E
+
+// Whch pin to monitor - default to 0
+static int adc_pin = 0;
+
+// conversion factor is into mili volts
+const float conversion_factor=3.3f*1000/(1<<12);
+
 
 int main() {
     // setup ADC
     stdio_init_all();
+    adc_init();
+    // set gpio as adc (no_pullups etc)
+    adc_gpio_init(26);
+    adc_select_input(adc_pin);
+
+    // Set up UART (debugging)
+    // Set up our UART with a basic baud rate.
+    uart_init(UART_ID, 2400);
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    int actual = uart_set_baudrate(UART_ID, BAUD_RATE);
+    // Set UART flow control CTS/RTS, we don't want these, so turn them off
+    uart_set_hw_flow(UART_ID, false, false);
+    // Set our data format
+    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+    // Turn off FIFO's - we want to do this character by character
+    uart_set_fifo_enabled(UART_ID, false);
 
     // i2c setup
-    // i2c_init(i2c1, 10000);
-    // i2c_set_slave_mode(i2c1, true, I2C_ADDR);
-    // gpio_set_function(2, GPIO_FUNC_I2C);
-    // gpio_set_function(3, GPIO_FUNC_I2C);
-    // gpio_pull_up(2);
-    // gpio_pull_up(3);
+    i2c_init(i2c1, 100000);
+    i2c_set_slave_mode(i2c1, true, I2C_ADDR);
+    gpio_set_function(2, GPIO_FUNC_I2C);
+    gpio_set_function(3, GPIO_FUNC_I2C);
+    gpio_pull_up(2);
+    gpio_pull_up(3);
+
+    //uart_puts(UART_ID, "Voltage-i2c on Pico");
     
     uint8_t rxdata[4];
     uint8_t txdata[2];
@@ -38,13 +77,19 @@ int main() {
         i2c_read_raw_blocking (i2c1, rxdata, 3);
         //sprintf (message, "Rx: %d %d %d\r\n", rxdata[0], rxdata[1], rxdata[2]);
         //uart_puts(UART_ID, message);
-        sprintf (message, "Value %d\r\n", rxdata[0]+(rxdata[1]<<8));
-
+        //sprintf (message, "Value %d\r\n", rxdata[0]+(rxdata[1]<<8));
+        sleep_ms(5);
+        //uart_puts(UART_ID, message);
+        
+        // Respond with ADC value (in milivolts)
+        uint16_t adc_value=10;
         // Note that this will drop fraction rather than round, but close enough
-        int value = (int) 10;
+        int value = (int) adc_value;
         txdata[0] = value & 0xFF;
         txdata[1] = value >> 8;
-        sprintf (message, "Tx: %d %d - %d\r\n", txdata[0], txdata[1], value);
+        //sprintf (message, "Tx: %d %d - %d\r\n", txdata[0], txdata[1], value);
+        sleep_ms(5);
+        //uart_puts(UART_ID, message);
         // Sends data in mv (as int)
         i2c_write_raw_blocking(i2c1, txdata, 2);
         
