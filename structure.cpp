f5713081ed7143beb91ee295257a4d4b859b351b
ShantamSridev <shantamsridev@gmail.com>
2024-03-15 19:26:59 +0000

    changed struct

--- structure.cpp
+++ structure.cpp
@@ -32,195 +32,247 @@ void add_to_fifo(int item) { //CORE1
 
 void getstruct(uint8_t addin, MyPID& pid, MyAngle& angle){
     switch(addin){
-        case 0:
+        case 0: {
             // Action for addin = 0
             break;
-        case I2C_GET_ANGLE:
-            // Action for addin = 4
+        }
+        case I2C_GET_ANGLE: {
+            // Action for addin = I2C_GET_ANGLE
             break;
-        case 8:
+        }
+        case 8: {
             // Action for addin = 8
             break;
-        case 12:
+        }
+        case 12: {
             // Action for addin = 12
             break;
-        case I2C_SETANGLE:
-            // Action for addin = 16
+        }
+        case I2C_SETANGLE: {
             float Requiredangle = memToFloat(I2C_SETANGLE);
-            angle.setAngle(Requiredangle,pid);
+            angle.setAngle(Requiredangle, pid);
             multicore_mem[I2C_SETANGLE] = Requiredangle;
             break;
-        case I2C_SETSPEED:
-            // Action for addin = 20
+        }
+        case I2C_SETSPEED: {
             float target = memToFloat(I2C_SETSPEED);
             pid.setTarget(target);
             multicore_mem[I2C_SETSPEED] = target;
             break;
-        case I2C_SETP:
-            // Action for addin = 24
+        }
+        case I2C_SETP: {
             float P = memToFloat(I2C_SETP);
             pid.setP(P);
             multicore_mem[I2C_SETP] = P;
             break;
-        case I2C_SETI:
-            // Action for addin = 28
+        }
+        case I2C_SETI: {
             float I = memToFloat(I2C_SETI);
             pid.setI(I);
             multicore_mem[I2C_SETI] = I;
             break;
-        case I2C_SETD:
-            // Action for addin = 32
+        }
+        case I2C_SETD: {
             float D = memToFloat(I2C_SETD);
             pid.setD(D);
             multicore_mem[I2C_SETD] = D;
             break;
-        case 36:
+        }
+        case 36: {
             // Action for addin = 36
             break;
-        case 40:
+        }
+        case 40: {
             // Action for addin = 40
             break;
-        case 44:
+        }
+        case 44: {
             // Action for addin = 44
             break;
-        case 48:
+        }
+        case 48: {
             // Action for addin = 48
             break;
-        case 52:
+        }
+        case 52: {
             // Action for addin = 52
             break;
-        case 56:
+        }
+        case 56: {
             // Action for addin = 56
             break;
-        case 60:
+        }
+        case 60: {
             // Action for addin = 60
             break;
-        case 64:
+        }
+        case 64: {
             // Action for addin = 64
             break;
-        case 68:
+        }
+        case 68: {
             // Action for addin = 68
             break;
-        case 72:
+        }
+        case 72: {
             // Action for addin = 72
             break;
-        case 76:
+        }
+        case 76: {
             // Action for addin = 76
             break;
-        case 80:
+        }
+        case 80: {
             // Action for addin = 80
             break;
-        case 84:
+        }
+        case 84: {
             // Action for addin = 84
             break;
-        case 88:
+        }
+        case 88: {
             // Action for addin = 88
             break;
-        case 92:
+        }
+        case 92: {
             // Action for addin = 92
             break;
-        case 96:
+        }
+        case 96: {
             // Action for addin = 96
             break;
-        case 100:
+        }
+        case 100: {
             // Action for addin = 100
             break;
-        case 104:
+        }
+        case 104: {
             // Action for addin = 104
             break;
-        case 108:
+        }
+        case 108: {
             // Action for addin = 108
             break;
-        case 112:
+        }
+        case 112: {
             // Action for addin = 112
             break;
-        case 116:
+        }
+        case 116: {
             // Action for addin = 116
             break;
-        case 120:
+        }
+        case 120: {
             // Action for addin = 120
             break;
-        case 124:
+        }
+        case 124: {
             // Action for addin = 124
             break;
-        case 128:
+        }
+        case 128: {
             // Action for addin = 128
             break;
-        case 132:
+        }
+        case 132: {
             // Action for addin = 132
             break;
-        case 136:
+        }
+        case 136: {
             // Action for addin = 136
             break;
-        case 140:
+        }
+        case 140: {
             // Action for addin = 140
             break;
-        case 144:
+        }
+        case 144: {
             // Action for addin = 144
             break;
-        case 148:
+        }
+        case 148: {
             // Action for addin = 148
             break;
-        case 152:
+        }
+        case 152: {
             // Action for addin = 152
             break;
-        case 156:
+        }
+        case 156: {
             // Action for addin = 156
             break;
-        case 160:
+        }
+        case 160: {
             // Action for addin = 160
             break;
-        case 164:
+        }
+        case 164: {
             // Action for addin = 164
             break;
-        case 168:
+        }
+        case 168: {
             // Action for addin = 168
             break;
-        case 172:
+        }
+        case 172: {
             // Action for addin = 172
             break;
-        case 176:
+        }
+        case 176: {
             // Action for addin = 176
             break;
-        case 180:
+        }
+        case 180: {
             // Action for addin = 180
             break;
-        case 184:
+        }
+        case 184: {
             // Action for addin = 184
             break;
-        case 188:
+        }
+        case 188: {
             // Action for addin = 188
             break;
-        case 192:
+        }
+        case 192: {
             // Action for addin = 192
             break;
-        case 196:
+        }
+        case 196: {
             // Action for addin = 196
             break;
-        case 200:
+        }
+        case 200: {
             // Action for addin = 200
             break;
-        case 204:
+        }
+        case 204: {
             // Action for addin = 204
             break;
-        case 208:
+        }
+        case 208: {
             // Action for addin = 208
             break;
-        case 212:
+        }
+        case 212: {
             // Action for addin = 212
             break;
-        case 216:
+        }
+        case 216: {
             // Action for addin = 216
             break;
-        case 220:
+        }
+        case 220: {
             // Action for addin = 220
             break;
-        case 224:
+        }
+        case 224: {
             // ONWARDS RESERVED FOR REQUEST
             break;
-        default:
-            // Default action if addin is not a multiple of 4 or out of range
-            printf("DONE GOOFED. Not a multiple of 4 or accessing somethign wrong.");
+        }
+        default: {
+            printf("DONE GOOFED. Not a multiple of 4 or accessing something wrong.");
             break;
+        }
     }
 }
